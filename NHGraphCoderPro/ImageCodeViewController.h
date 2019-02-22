4f671c41c1047f9124417919bef2e894af95dbcf
zhangshuai <529155970@qq.com>
2019-02-22 22:25:43 +0800

    add new vc

---
+++ NHGraphCoderPro/ImageCodeViewController.h
@@ -0,0 +1,17 @@
+//
+//  ImageCodeViewController.h
+//  NHGraphCoderPro
+//
+//  Created by 张帅 on 2019/2/22.
+//  Copyright © 2019 hu jiaju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ImageCodeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NHGraphCoderPro/ImageCodeViewController.m
@@ -0,0 +1,33 @@
+//
+//  ImageCodeViewController.m
+//  NHGraphCoderPro
+//
+//  Created by 张帅 on 2019/2/22.
+//  Copyright © 2019 hu jiaju. All rights reserved.
+//
+
+#import "ImageCodeViewController.h"
+
+@interface ImageCodeViewController ()
+
+@end
+
+@implementation ImageCodeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
